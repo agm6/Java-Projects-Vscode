4926a9ad9be41377b7d7af36041f30a32d8eb733
agm6 <agm6@hw.ac.uk>
2024-08-25 16:04:58 +0100

    Created the contact.java class to use as an object for the values of the hashmap.

---
+++ Phonebook/Contact.java
@@ -0,0 +1,13 @@
+public class Contact {
+    
+    private String contactName;
+    private int phoneNumber;
+    private String contactEmail;
+
+    public Contact (String name, int number, String email){
+        contactName = name;
+        phoneNumber = number;
+        contactEmail = email;
+    }
+
+}
