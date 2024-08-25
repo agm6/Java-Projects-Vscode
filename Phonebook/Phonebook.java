dc132b50c291ca579a2bbf76859e0311d3df84f4
agm6 <agm6@hw.ac.uk>
2024-08-25 17:47:37 +0100

    Added option to either look up existing person in the phonebook or add a new one.

--- Phonebook/Phonebook.java
+++ Phonebook/Phonebook.java
@@ -10,19 +10,30 @@ public class Phonebook{
         phoneBook.put("Jim James", new Contact("Jim James", 11, "jim@james"));
         phoneBook.put("Jill James", new Contact("Jill James", 12, "jill@james"));
 
+
+        System.out.println("Would you like to:\n1)Look up someone.\n2)Add a new contact.");
+
         Scanner n = new Scanner(System.in);
-        System.out.println("Enter name, number, email");
-        String name2 = n.nextLine();
-        int number2 = Integer.parseInt(n.nextLine());
-        String email2 = n.nextLine();
-        
-        //n.close();
+        int choice = Integer.parseInt(n.nextLine());
 
-        phoneBook.put(name2, new Contact(name2, number2, email2));
+        if(choice == 1){
+            System.out.println("Please enter the name of the person you are looking for:");
+            String name = n.nextLine();
+            //System.out.println(name);
+            Contact var = phoneBook.get(name);
+            System.out.println(var.toString());
+        }
+        else{
+            System.out.println("Enter name, number, email");
+            String name2 = n.nextLine();
+            int number2 = Integer.parseInt(n.nextLine());
+            String email2 = n.nextLine();
+            //n.close();
 
+            phoneBook.put(name2, new Contact(name2, number2, email2));
 
-
-        Contact var = phoneBook.get(name2);
-        System.out.println(var.getNumber());
+            Contact var = phoneBook.get(name2);
+            System.out.println(var.toString());
+        }
     }
 }
