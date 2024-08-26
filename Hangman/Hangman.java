625e73db46dcd62ee55469ca33e625603afb3b59
agm6 <agm6@hw.ac.uk>
2024-08-26 17:04:19 +0100

    Added simple functionality to compare two strings.

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -1,10 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//LOGIC: compare first array to array made from user guess and return the letters that are correct.
 
 public class Hangman{
     public static void main(String[] args){
         String wordToGuess = "apple";
         String[] letters = wordToGuess.split("");
-        for(String s : letters){
+        /*for(String s : letters){
             System.out.println(s);
+        }*/
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your guess: ");
+        String guess = scan.nextLine();
+        String[] guessLetters = guess.split("");
+
+        if(Arrays.equals(letters, guessLetters)){
+            System.out.println("You were correct!");
         }
+        else{
+            System.out.println("You were wrong!");
+        }
+
     }
 }
