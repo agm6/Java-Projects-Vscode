4b45cd64f9779dc92aecf1e651a371d832a138d7
agm6 <agm6@hw.ac.uk>
2024-08-26 20:33:50 +0100

    Changed implementation to compare a letter against each in the word, though the loops need to be fixed.

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -5,23 +5,45 @@ import java.util.Scanner;
 
 public class Hangman{
     public static void main(String[] args){
+
         String wordToGuess = "apple";
         String[] letters = wordToGuess.split("");
-        /*for(String s : letters){
-            System.out.println(s);
-        }*/
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your guess: ");
-        String guess = scan.nextLine();
-        String[] guessLetters = guess.split("");
+        String[] wordDisplayed = new String[letters.length];
+        for(int i = 0; i < wordDisplayed.length; i++){
+            wordDisplayed[i] = "_";
+        }
 
-        if(Arrays.equals(letters, guessLetters)){
-            System.out.println("You were correct!");
-        }
-        else{
-            System.out.println("You were wrong!");
-        }
+        boolean blanks = false;
+
+        while(true){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter your guess: ");
+            String guess = scan.nextLine();
+
+            for(int i = 0; i < letters.length; i++){
+                if(letters[i].equals(guess)){
+                    System.out.println("You guessed correct!");
+                    wordDisplayed[i].equals(guess);
+                    System.out.println(Arrays.toString(wordDisplayed));
+
+                    for(int j = 0; j < wordDisplayed.length; i++){
+                        if(wordDisplayed[j].equals("_")){
+                            blanks = true;
+                        }
+                    }
+                    if(!blanks){
+                        System.out.println("You win!");
+                        break;
+                    }
+
+                }
+                else{
+                    System.out.println("You guessed wrong!");
+                    continue;
+                }
+            }
+         }
 
     }
 }
